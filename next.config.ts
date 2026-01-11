64a7b10fec14ad581f753d113eeccd7eed628ba8
saurabhkansal <saurabhkansal101@gmail.com>
2026-01-11 21:17:52 +0530

    prev

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Essential for serverless functions to skip minification of these binaries
   serverExternalPackages: ['puppeteer-core', '@sparticuz/chromium'],
+  
   outputFileTracingIncludes: {
-    '/api/**/*': ['./node_modules/@sparticuz/chromium/bin/**/*'],
+    '/api/**/*': [
+      './node_modules/@sparticuz/chromium/bin/**/*'
+    ],
   },
 };
 
